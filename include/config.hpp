ecbdd59f7202c182b04d876618c87a3e7d744079
P-S-Y-S-U <mohan.gaming78@gmail.com>
2024-07-10 13:51:54 +0530

    vkrender config headed

---
+++ include/config.hpp
@@ -0,0 +1,10 @@
+#ifndef VKRENDER_CONFIG_HPP
+#define VKRENDER_CONFIG_HPP
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtx/hash.hpp>
+
+#endif
